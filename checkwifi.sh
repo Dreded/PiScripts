365038c4691826fb00f3def734ff79d5cfb8668b
Dreded <dreded@dredx.com>
2018-02-04 14:36:44 -0800

    Few More Scripts

--- checkwifi.sh
+++ checkwifi.sh
@@ -4,15 +4,45 @@
 #add to crontab with command: 
 #*/5 * * * * /usr/local/bin/checkwifi.sh >> /home/pi/wifi.log 2>&1
 
-ping -c4 10.1.1.1 > /dev/null
- 
-if [ $? != 0 ] 
-then
-  echo "No network connection, restarting" | ts '[%Y-%m-%d %H:%M:%S]'
-  sudo /sbin/shutdown -r now
-else
-  echo "Network is Connected." | ts '[%Y-%m-%d %H:%M:%S]'
+usage="
+  Usage:
+    checkwifi.sh -r will run the script
+    checkwifi.sh -i will install the script
+"
+
+croncmd="/usr/local/bin/checkwifi.sh -r >> /home/pi/wifi.log 2>&1"
+cronjob="*/5 * * * * $croncmd"
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
 fi
+
+if  [[ $1 = "-i" ]]; then
+  #The below section will copy the script to  BINDIR
+  DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+  BINDIR="/usr/local/bin"
+  if [[ $DIR != $BINDIR && -w $BINDIR ]]; then
+    echo "This script is being run outside of $BINDIR so will be installed"
+    cp "${BASH_SOURCE[0]}" $BINDIR
+    #add to crontab will not make duplicate entries
+    ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
+    #if needed later the below command would remove from crontab
+    ( crontab -l | grep -v -F "$croncmd" ) | crontab -
+  fi
+  elif [[ $1 = "-r" ]]; then
+    ping -c4 10.1.1.1 > /dev/null
+
+    if [ $? != 0 ]; then
+      echo "No network connection, restarting" | ts '[%Y-%m-%d %H:%M:%S]'
+      sudo /sbin/shutdown -r now
+    else
+      echo "Network is Connected." | ts '[%Y-%m-%d %H:%M:%S]'
+    fi
+  else
+    echo "$usage"
+fi
+
 #then
 #  echo "No network connection, restarting wlan0" | ts '[%Y-%m-%d %H:%M:%S]'
 #  /sbin/ifdown 'wlan0'
